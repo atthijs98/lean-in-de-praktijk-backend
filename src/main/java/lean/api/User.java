db978ddd661252e6b13196759edec7ee33b8452a
Matthijs <matthijsvaneijk@gmail.com>
2020-08-20 15:15:05 +0200

    fixed bug in usermapper

--- src/main/java/lean/api/User.java
+++ src/main/java/lean/api/User.java
@@ -1,9 +1,15 @@
 package lean.api;
 
+
 import lean.util.MessageFactory.Language;
 
 import java.security.Principal;
 
+/**
+ * User class, represents the user entity in the data model.
+ *
+ * @author matthijs
+ */
 public class User implements Principal {
     private int id;
     private String UUID;
@@ -13,72 +19,68 @@ public class User implements Principal {
     private String authToken;
     private Language language;
 
-    public User(int id, String uuid, String email, String name, int privilege, Language language){}
+    public User() {}
 
-    public User(int id, String UUID, String email, String name, int privilege, String authToken, Language language) {
+    public User(int id, String UUID, String email, String name, int privilege, Language language) {
         this.id = id;
         this.UUID = UUID;
         this.email = email;
         this.name = name;
         this.privilege = privilege;
-        this.authToken = authToken;
         this.language = language;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getPrivilege() {
-        return privilege;
-    }
-
-    public Language getLanguage() {
-        return language;
-    }
-
-    public String getAuthToken() {
-        return authToken;
     }
 
     public String getUUID() {
         return UUID;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
+    public String getEmail() {
+        return email;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setLanguage(Language language) {
-        this.language = language;
+    public String getName() {
+        return name;
+    }
+
+    public int getPrivilege() {
+        return privilege;
     }
 
     public void setPrivilege(int privilege) {
         this.privilege = privilege;
     }
 
-    public void setUUID(String UUID) {
-        this.UUID = UUID;
+    public String getAuthToken() { return authToken; }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
     }
 }
